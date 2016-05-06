e81a24861546a84ba84ba43f9e5aacad89fbb8ca
Anthony Simpson <ansi6622@colorado.edu>
2016-05-06 14:50:15 -0600

    running nodemon keeps me from from commiting often, i should just run another tab for that sort of thing

--- client/services/movie.service.js
+++ client/services/movie.service.js
@@ -1,6 +1,32 @@
 angular.module('omdbMovieApp')
 .factory('movieDataService', function($http){
-  var moviesArray = [];
+  var moviesArray = [{
+        "id": 1,
+        "favorite": false,
+        "date": moment( new Date().getTime() - 285678 ).fromNow(),
+        "votes": 5,
+        "comments": [],
+        "Title": "The Matrix",
+        "Year": "1999",
+        "Rated": "R",
+        "Released": "31 Mar 1999",
+        "Runtime": "136 min",
+        "Genre": "Action, Sci-Fi",
+        "Director": "Lana Wachowski, Lilly Wachowski",
+        "Writer": "Lilly Wachowski, Lana Wachowski",
+        "Actors": "Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss, Hugo Weaving",
+        "Plot": "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+        "Language": "English",
+        "Country": "USA, Australia",
+        "Awards": "Won 4 Oscars. Another 33 wins & 44 nominations.",
+        "Poster": "http://ia.media-imdb.com/images/M/MV5BMTkxNDYxOTA4M15BMl5BanBnXkFtZTgwNTk0NzQxMTE@._V1_SX300.jpg",
+        "Metascore": "73",
+        "imdbRating": "8.7",
+        "imdbVotes": "1,185,648",
+        "imdbID": "tt0133093",
+        "Type": "movie",
+        "Response": "True"
+      }];
   var factory = {
     movies: moviesArray,
     movieById: function(movieId){
